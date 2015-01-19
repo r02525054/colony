fa910bb80f32634aab8c8c5ea82a63aeb39b31c4
r02525054 <r02525054@ntu.edu.tw>
2015-01-19 13:19:59 +0800

    1.加上新的crop library 2.加上calendarpicker library 3.加上config class
    Signed-off-by: r02525054 <r02525054@ntu.edu.tw>

---
+++ ColonyCountV2.0/src/com/colonycount/cklab/config/Config.java
@@ -0,0 +1,9 @@
+package com.colonycount.cklab.config;
+
+public class Config {
+	public static final int CROP_REQUEST_WIDTH = 1024;
+	public static final int CROP_REQUEST_HEIGHT = 1024;
+	
+	public static final int OUTPUT_IMAGE_WIDTH = 1024;
+	public static final int OUTPUT_IMAGE_HEIGHT = 1024;
+}
---
+++ ColonyCountV2.0/src/com/colonycount/cklab/croptest/OnDragCallback.java
@@ -0,0 +1,5 @@
+package com.colonycount.cklab.croptest;
+
+public interface OnDragCallback {
+	void onDragFinish(float dx, float dy);
+}
